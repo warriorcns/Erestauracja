19779cfa8fb5938e4d280d10ebd601bc1619e96c
warriorcns@gmail.com <warriorcns@gmail.com@08ce201e-e56c-43f5-8e04-531f4f6d07aa>
2012-08-03 19:13:29 +0000

    To co bylo wczesniej + dzialajace logowanie na podstawie niezmodyfikowanego przykladu.
    git-svn-id: https://pl5.projectlocker.com/Erestauracja/Inz/svn/Erestauracja@47 08ce201e-e56c-43f5-8e04-531f4f6d07aa

---
+++ Erestauracja/Models/modeltest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Erestauracja.Models
+{
+    public class modeltest
+    {
+        [Display(Name = "User name")]
+        public string Name { get; set; }
+    }
+}
