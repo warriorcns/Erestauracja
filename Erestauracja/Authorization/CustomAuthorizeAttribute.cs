606d6a1b92c30ed4313a4453ee4a28b10edacd7a
warriorcns@gmail.com <warriorcns@gmail.com@08ce201e-e56c-43f5-8e04-531f4f6d07aa>
2012-09-05 12:22:15 +0000

    zmiana nazwy bez spacji na koncu
    git-svn-id: https://pl5.projectlocker.com/Erestauracja/Inz/svn/Erestauracja@124 08ce201e-e56c-43f5-8e04-531f4f6d07aa

---
+++ Erestauracja/Authorization/CustomAuthorizeAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Security;
+using System.Web.Routing;
+
+namespace Erestauracja.Authorization
+{
+
+
+    public class CustomAuthorizeAttribute : AuthorizeAttribute
+    {
+
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            base.OnAuthorization(filterContext);
+            if (!filterContext.HttpContext.User.Identity.IsAuthenticated)
+            {
+                filterContext.Result = new RedirectResult("~/Account/Logon");
+                return;
+            }
+
+            if (filterContext.Result is HttpUnauthorizedResult)
+            {
+                filterContext.Result = new RedirectResult("~/Home/Unauthorized");
+                return;
+            }
+        } 
+    }
+}
